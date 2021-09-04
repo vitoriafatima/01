6b66968932851d4b084071129a882940b7d03526
vitoriafatima <vitoriatorralvo.fatima@gmail.com>
2021-09-04 17:22:00 -0300

    01

---
+++ _javascript/01.js
@@ -0,0 +1,25 @@
+//comentário de linha
+
+//console.log('olá mundo!!');
+
+var nome = 'vitoria';
+var sobrenome = 'fatima';
+var idade = '23'
+var nomecompleto = nome+sobrenome+idade;
+//console.log(nomecompleto);
+
+var valorum = 10;
+var valordois = 2;
+var total;
+//total = valorum+valordois;
+//total = valorum - valordois;
+//total = valorum * valordois;
+//total = valorum / valordois;
+//console.log(total);
+total = valorum ** valordois;
+
+console.log(total);
+
+
+
+
---
+++ _javascript/caculo.js
@@ -0,0 +1,7 @@
+var valorum = 4;
+var valordois = 5;
+var valortres = 9;
+var total = (valorum + valordois) * valortres;
+
+console.log(total);
+
